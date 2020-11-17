367e12b1bcd43bfb71921121c88a1f7372f93f7c
ShimizuNaoki <sensuikan1973@gmail.com>
2020-11-18 01:50:04 +0900

    improve gen_toc script

--- scripts/gen_toc.sh
+++ scripts/gen_toc.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # 目次生成
-./gh-md-toc --insert README.md
-./gh-md-toc --insert translations/Learning_to_Play_Othello_with_Deep_Neural_Networks/README.md
-./gh-md-toc --insert translations/Developing_an_Artificial_Intelligence_for_Othello/README.md
+./gh-md-toc --insert README.md \
+translations/Learning_to_Play_Othello_with_Deep_Neural_Networks/README.md \
+translations/Developing_an_Artificial_Intelligence_for_Othello/README.md
+
+# 一時ファイルを削除
 find ./ -name "*.orig.*" | xargs rm
 find ./ -name "*.toc.*" | xargs rm
