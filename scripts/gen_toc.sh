667eba58801170bc661adfd0fc0eee80629e19e4
ShimizuNaoki <sensuikan1973@gmail.com>
2020-11-23 04:35:41 +0900

    fix script

--- scripts/gen_toc.sh
+++ scripts/gen_toc.sh
@@ -10,5 +10,5 @@
 cd translations/Developing_an_Artificial_Intelligence_for_Othello
 ./../../gh-md-toc --insert --no-backup README.md
 
-cd translations/Learning_to_Play_Othello_with_Deep_Neural_Networks
+cd ../Learning_to_Play_Othello_with_Deep_Neural_Networks
 ./../../gh-md-toc --insert --no-backup README.md
