db269a1649f44d3f15aa234cf079757cd237e08c
ShimizuNaoki <sensuikan1973@gmail.com>
2020-12-05 22:14:35 +0900

    drop unnecessary docs

--- scripts/archived/calc_state_complexity.sh
+++ scripts/archived/calc_state_complexity.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 # オセロの state-space complexity を概算する script
-# 各種文献で 10^28 という主張が書かれてるが、導出過程が書かれてない。というわけで、それをやる script を用意した。
-# ^ の各種文献
 # * [Searching for Solutions in Games and Artificial Intelligence](http://fragrieu.free.fr/SearchingForSolutions.pdf)
 # * [Optimizing Search Space of Othello Using Hybrid Approach](https://www.researchgate.net/publication/289505529_Optimizing_Search_Space_of_Othello_Using_Hybrid_Approach)
 #
@@ -26,13 +24,8 @@ has_illegal_center=`bc_l "$center_four_squares_empty + $center_three_squares_emp
 ## 初手の制約に違反している局面数の算出
 no_stones_at_first_move=`bc_l '(2^4)*(3^58 - 1)*4'` # 初手で着手されるはずの箇所に石がないパターン: d4,d5,e4,e5 のパターン数 * (c4,d3,e6,f5 が空きマスのパターン - 他が全部空のパターン)
 ## オセロで石が孤立することはないが、それに違反している局面数の算出
-has_stones_in_middle_edge_and_edge_exclude_box_edge=`bc_l '(2^4)*(3^44 - 1)'` # ボックス内の辺に石がないが、中辺あるいは辺に石がある状態: 中辺あるいは辺だけに石があるパターン数。全部空きマスのパターンを差し引く。
-has_stones_in_box_edge_and_edge_exclude_middle_edge=`bc_l '(2^4)*(3^40 - 1)'` # 中辺に石がないが、ボックス内の辺あるいは辺に石がある状態: ボックス内の辺あるいは辺だけに石があるパターン数。全部空きマスのパターンを差し引く。
-has_uncorrect_arranged_stones_in_box_edge_and_middle_edge=`bc_l '(2^4)*( (2^3) + (2* (3^3 - 1))*2)*(3^45)*4'` # ボックス内の辺と中辺に石があるが、その並びが成立しえない状態: (d3,c4,b2) + (c4, b2,c2,d2)*反転
-# ボックス内の辺と中辺に石があるが、その並びが成立しえない状態: (c4,d3,b2,辺) + (c4, 辺+中辺)*反転 + (c3,c4,b4)*反転 + (c3,c4,b3)*反転 + (c3,c4,b3,b4)*反転 + (c3,c4,b3,b4,b5)*反転 + (c3,c4,b3,b4)*反転 + (c3,c4,b3,b4)*反転
-# 頭こんがらがってきたので、memo.md にまとめ中
-has_uncorrect_arranged_stones_in_all_edge=`bc_l '(2^4)*( ((2^3)*(3^7 - 1)) + (2*(2^3)*(3^7 - 1))*2 + ((2^3)*(3^5 - 1))*2 + ((2^3)*(3^4 - 1))*2 + ((2^4)*(3^4 - 1))*2 + ((2^5)*(2^1))*2 + ((2^5)*(2^1))*2 + ((2^5)*(2^1))*2 )*(3^45)*4'`
-has_isolated_stones=`bc_l "$has_stones_in_middle_edge_and_edge_exclude_box_edge + $has_stones_in_box_edge_and_edge_exclude_middle_edge + $has_uncorrect_arranged_stones_in_box_edge_and_middle_edge"`
+# これを数式的に表現できない。モンテカルロでがんばってパターン数削るしかない。
+# See: [Optimizing Search Space of Othello Using Hybrid Approach](https://www.researchgate.net/publication/289505529_Optimizing_Search_Space_of_Othello_Using_Hybrid_Approach)
 
 illegal_cases=`bc_l "$has_illegal_center + $no_stones_at_first_move + $has_isolated_stones"`
 
